a17b41f10c335ebfbf1d87cb4a5721dc991035c8
Raymundo T. Melecio <raymundo@espoc.com>
2021-02-06 00:17:24 +0800

    update code

--- lib/src/day_view/day_view_essentials/day_view_essentials.dart
+++ lib/src/day_view/day_view_essentials/day_view_essentials.dart
@@ -90,6 +90,6 @@ class _DayViewEssentialsInherited extends InheritedWidget {
   }
 
   static _DayViewEssentialsInherited of(BuildContext context) {
-    return context dependOnInheritedWidgetOfExactType<_DayViewEssentialsInherited>();
+    return context.dependOnInheritedWidgetOfExactType<_DayViewEssentialsInherited>();
   }
 }
